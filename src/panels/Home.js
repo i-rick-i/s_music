17e1ba9bacf4e6f64267379e2042c58ec2d1efe9
i-rick-i <83608226+i-rick-i@users.noreply.github.com>
2022-02-04 14:09:19 +0300

    c1

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -16,7 +16,7 @@ const Home = ({ id, go, fetchedUser }) => (
 		<Group header={<Header mode="secondary">Navigation Example</Header>}>
 			<Div>
 				<Button stretched size="l" mode="secondary" onClick={go} data-to="persik">
-					Show me the Persik, please
+					ку]
 				</Button>
 			</Div>
 		</Group>
